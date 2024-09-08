7f9228e3eda437a45322fe737b558115e1b5e5a1
Mirislom <m.mirislom.m@gmail.com>
2024-09-08 22:32:03 +0500

    [+]RhymeHistoryCard
    Вынесли в отдельную папку RhymeHistoryCard

---
+++ lib/features/search/widgets/rhyme_history_card.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:rhymer2/ui/ui.dart';
+
+class RhymeHistoryCard extends StatelessWidget {
+  const RhymeHistoryCard({super.key, required this.rhymes});
+
+  final List<String> rhymes;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    return BaseContainer(
+      width: 200,
+      padding: EdgeInsets.all(16),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            "Рифма",
+            style: theme.textTheme.bodyLarge?.copyWith(
+              fontWeight: FontWeight.w700,
+            ),
+          ),
+          Wrap(
+            children: rhymes
+                .map((e) => Padding(
+                      padding: EdgeInsets.only(right: 6),
+                      child: Text(e),
+                    ))
+                .toList(),
+          ),
+        ],
+      ),
+    );
+  }
+}
